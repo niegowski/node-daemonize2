1f6858cfe023df4500e392788c07fdc2b251c89e
Kuba Niegowski <kuba@niegowski.pl>
2012-01-25 00:03:13 +0100

    gracefull terminate in example app

--- examples/sample.js
+++ examples/sample.js
@@ -3,19 +3,32 @@ var http = require("http"),
     util = require("util");
 
 
-http.createServer(function(req, res) {
-    util.log("Req from " + req.connection.remoteAddress);
+var server = http.createServer()
+    .on("request", function(req, res) {
+        util.log("Req from " + req.connection.remoteAddress);
+        
+        res.writeHead(200, {
+            "Content-Type": "text/plain"
+        });
+        
+        res.end("Hello World");
+        
+    })
+    .on("close", function() {
+        process.exit(0);
+    })
+    .listen(8080);
     
-    res.writeHead(200, {
-        "Content-Type": "text/plain"
-    });
-    
-    res.end("Hello World");
-    
-}).listen(8080);
 
 util.log("Started");
 
+
 process.on("SIGUSR1", function() {
     util.log("Reloading...");
+});
+
+process.once("SIGTERM", function() {
+    util.log("Stopping...");
+
+    server.close();
 });
