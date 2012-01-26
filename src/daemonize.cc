715df30993d4b7617e67faf567b47ab9679c9d56
Kuba Niegowski <kuba@niegowski.pl>
2012-01-26 02:08:34 +0100

    code refactor

--- src/daemonize.cc
+++ src/daemonize.cc
@@ -1,4 +1,4 @@
-// Copyright (c) 2011 Kuba Niegowski
+// Copyright (c) 2012 Kuba Niegowski
 // 
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
